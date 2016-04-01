beefe86cde9edde4128cb2fd86677d6ea0ed0c6d
BKhurshid <josephsun4@gmail.com>
2016-03-31 18:26:43 -0700

    imported router, route, hashhistory from react-router

--- lessons/02-rendering-a-route/index.js
+++ lessons/02-rendering-a-route/index.js
@@ -1,4 +1,6 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './modules/App'
+import React from 'react';
+import { render } from 'react-dom';
+import App from './modules/App';
+import {Router, Route, hashHistory} from 'react-router';
+
 render(<App/>, document.getElementById('app'))

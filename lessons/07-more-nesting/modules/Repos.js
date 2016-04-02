25153b0da6155902a5996e13b6838d39f0ad323c
BKhurshid <josephsun4@gmail.com>
2016-04-02 11:58:54 -0700

    Created Home component

--- lessons/07-more-nesting/modules/Repos.js
+++ lessons/07-more-nesting/modules/Repos.js
@@ -5,12 +5,13 @@ export default React.createClass({
   render() {
     return (
       <div>
-        <h2>Repos</h2>
-        <ul>
-          <li><Link to="/repos/reactjs/react-router">React Router</Link></li>
-          <li><Link to="/repos/facebook/react">React</Link></li>
-        </ul>
-      </div>
+  <h2>Repos</h2>
+  <ul>
+    <li><Link to="/repos/reactjs/react-router">React Router</Link></li>
+    <li><Link to="/repos/facebook/react">React</Link></li>
+  </ul>
+  {this.props.children}
+</div>
     )
   }
 })

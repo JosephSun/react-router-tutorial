596eeec0c2b6e97b7509aad8ade4d9f03c31a175
BKhurshid <josephsun4@gmail.com>
2016-04-02 13:19:21 -0700

    put in tutorials return for render

--- lessons/12-navigating/modules/Repos.js
+++ lessons/12-navigating/modules/Repos.js
@@ -14,22 +14,23 @@ export default React.createClass({
 
   render() {
     return (
-      <div>
-        <h2>Repos</h2>
-        <ul>
-          <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
-          <li><NavLink to="/repos/facebook/react">React</NavLink></li>
-          <li>
-            <form onSubmit={this.handleSubmit}>
-              <input type="text" placeholder="userName"/> /{' '}
-              <input type="text" placeholder="repo"/> /{' '}
-              <button type="submit">Go</button>
-            </form>
-          </li>
-        </ul>
-        {this.props.children}
-      </div>
-    )
+          <div>
+            <h2>Repos</h2>
+            <ul>
+              <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
+              <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+              {/* add this form */}
+              <li>
+                <form onSubmit={this.handleSubmit}>
+                  <input type="text" placeholder="userName"/> / {' '}
+                  <input type="text" placeholder="repo"/>{' '}
+                  <button type="submit">Go</button>
+                </form>
+              </li>
+            </ul>
+            {this.props.children}
+          </div>
+        )
   }
 
 })

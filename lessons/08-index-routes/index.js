cbfc0c75f4d8282efc209fb403e1cd33215c13c5
BKhurshid <josephsun4@gmail.com>
2016-04-02 12:01:32 -0700

    imported home component and index route compponent

--- lessons/08-index-routes/index.js
+++ lessons/08-index-routes/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
 import Repo from './modules/Repo'
+import Home from './modules/Home'
 
 render((
   <Router history={hashHistory}>

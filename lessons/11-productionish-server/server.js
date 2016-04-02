05f99158cfea3cf885a1733ac05076404f3e3aea
BKhurshid <josephsun4@gmail.com>
2016-04-02 12:55:03 -0700

    listening to port

--- lessons/11-productionish-server/server.js
+++ lessons/11-productionish-server/server.js
@@ -9,3 +9,10 @@ app.use(express.static(__dirname));
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+
+const PORT = process.env.port || 8080;
+
+app.listen(PORT, function() {
+  console.log('Production Express server running at localhost:' + PORT);
+});

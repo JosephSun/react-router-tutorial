300688512d2632d7654d25c2f13a34e24d898b14
BKhurshid <josephsun4@gmail.com>
2016-04-02 13:04:28 -0700

    added minification

--- lessons/11-productionish-server/webpack.config.js
+++ lessons/11-productionish-server/webpack.config.js
@@ -11,5 +11,10 @@ module.exports = {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
     ]
-  }
+  },
+  plugins: process.env.NODE_ENV === 'production' ? [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.OccurrenceUglifyJsPlugin()
+  ] : [],
 }

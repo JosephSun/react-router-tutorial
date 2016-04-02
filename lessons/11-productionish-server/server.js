8657d4b7db94ea5cc97f3192e8c1b63f793eac00
BKhurshid <josephsun4@gmail.com>
2016-04-02 12:54:03 -0700

    served the files as static and send all requests to index.html

--- lessons/11-productionish-server/server.js
+++ lessons/11-productionish-server/server.js
@@ -1,3 +1,11 @@
 import express from 'express';
 import path from 'path';
 import compression from 'compresson';
+
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});

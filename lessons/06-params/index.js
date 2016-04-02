0e4094fdc1091757fa971a63efb49d8eee01d096
BKhurshid <josephsun4@gmail.com>
2016-04-01 18:39:35 -0700

    added params repo link

--- lessons/06-params/index.js
+++ lessons/06-params/index.js
@@ -9,7 +9,8 @@ render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
       <Route path="/repos" component={Repos}/>
+      <Route path="/repos/:userName/:repoName" component={Repos}/>
       <Route path="/about" component={About}/>
     </Route>
   </Router>
-), document.getElementById('app'))
+), document.getElementById('app'));

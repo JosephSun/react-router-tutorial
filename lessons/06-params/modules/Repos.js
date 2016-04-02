cc5f86bd6033a8c183f38eb76131fee0fe69a559
BKhurshid <josephsun4@gmail.com>
2016-04-01 18:41:42 -0700

    added new links for params

--- lessons/06-params/modules/Repos.js
+++ lessons/06-params/modules/Repos.js
@@ -4,6 +4,12 @@ export default React.createClass({
   render() {
     return (
       <div>
+        <h2>Repos</h2>
+
+        <ul>
+          <li><Link to="repos/reactjs/react-router">React Router</Link></li>
+          <li><Link to="/repos/facebook/react">React</Link></li>
+        </ul>
         <h2>{this.props.params.repoName}</h2>
       </div>
   )
